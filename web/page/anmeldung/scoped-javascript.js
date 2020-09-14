fefa390363aac0352b195d7168f88c915b6cc1db
Yamilka Alvarez <yamilka.alvarez@topdev.de>
2020-09-14 10:52:50 +0200

    Anmeldung-Seite + Textfield-Component

---
+++ web/page/anmeldung/scoped-javascript.js
@@ -0,0 +1,17 @@
+new Vue({
+    el:'#as_page_anmeldung',
+    data:
+    {
+        user:{value:"", error:""},
+        passwort:{value:"", error:""}
+    },
+    computed:
+    {
+
+    },
+    method:
+    {
+
+    }
+
+});
