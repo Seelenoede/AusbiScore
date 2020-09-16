17900d775edd92eb3011fc0b2e4873678a40605d
Yamilka Alvarez <yamilka.alvarez@topdev.de>
2020-09-16 21:32:44 +0200

    Register-Seite + Logo-Component

---
+++ web/components/banner/component.js
@@ -0,0 +1,17 @@
+Vue.component('as-banner', {
+    props:
+        {
+        },
+    data: function ()
+    {
+        return {}
+    },
+    computed:
+        { },
+    methods:
+        {},
+    template: '' +
+        '<div class="as_banner">' +
+            '<a href=""><img src="../../medien/logo.svg" alt=""></a>'+
+        '</div>'
+});
---
+++ web/page/register/scoped-javascript.js
@@ -0,0 +1,42 @@
+new Vue({
+    el:'#as_page_register',
+    data:
+    {
+        as_tabs:
+        [
+            {active:true, visible:false, disabled:false, loaded:false, errorText:"", name:"tab1", label:"Registrieren"}
+        ],
+        benutzer:
+        {
+            name:{value:"", error:""},
+            vorname:{value:"", error:""},
+            klasse:{value:"", error:""},
+            unternehmen:{value:"", error:""},
+            userName:{value:"", error:""},
+            passwort:{value:"", error:""},
+        }
+    },
+    mounted:function()
+    {},
+    computed:
+    {},
+    methods:
+    {
+        passwortAnzeigen:function()
+        {
+            let input= $("#as_page_register #asl_eye_passwort input");
+            if (input.attr("type") === "password")
+            {
+                input.attr("type", "text");
+            }
+            else
+            {
+                input.attr("type", "password");
+            }
+        },
+        speichern:function () {
+            // hier wird den Eintrag gespecihert
+        }
+    }
+
+});
