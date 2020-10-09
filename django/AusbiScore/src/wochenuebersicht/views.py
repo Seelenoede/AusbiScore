b810e3fcd15956fd71601d153b2e0bfdb9a7cf71
Larissa Kähl <lassari.dev@gmail.com>
2020-10-09 11:28:36 +0200

    clean wochenuebersicht code

--- django/AusbiScore/src/wochenuebersicht/views.py
+++ django/AusbiScore/src/wochenuebersicht/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from django.db import connection
-from json import dumps
-from datetime import datetime
 
 def uebersicht_view(request):
     return render(request, 'page/wochenuebersicht/page.html', {})
-
 
 def query_function(query):
     cur = connection.cursor()
@@ -16,8 +13,6 @@ def query_function(query):
 def uebersicht_js_view(request):
     bericht_data = query_function('SELECT `Tag`, DATE_FORMAT(`Datum`, "%Y-%m-%d") as Datum, `Ist_Vollständig` FROM bericht')
     print(bericht_data)
-
-
 
     return render(request, 'page/wochenuebersicht/scoped-javascript.js',{"bericht_data": bericht_data})
 
