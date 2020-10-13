546f37f7289681b70d2b1134001f11349a015167
Larissa Kähl <lassari.dev@gmail.com>
2020-10-13 09:32:18 +0200

    clean settings code

--- django/AusbiScore/src/mysite/settings.py
+++ django/AusbiScore/src/mysite/settings.py
@@ -83,6 +83,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 Database
 https://docs.djangoproject.com/en/2.2/ref/settings/#databases
