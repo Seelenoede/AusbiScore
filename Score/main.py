58923a470c7f5daba0b0da58cc5a061ae01f4b46
Larissa Kähl <lassari.dev@gmail.com>
2020-10-14 17:31:16 +0200

    cleaned the code and add comments

--- Score/main.py
+++ Score/main.py
@@ -4,24 +4,24 @@ import os
 input_dir = os.listdir("input")
 
 
-def get_score_ziel(a, b):
-    current_score = a * b
+def get_score_ziel(current_specific_score, gewicht):  # current_specific_score: all scores you can receive for "Ziel"
+    current_score = current_specific_score * gewicht
     return current_score
 
 
-def get_score_berichtshet(a, b):
-    if a == "ja":
-        return b
+def get_score_berichtshet(taeglich_oder_puenktlich, score):
+    if taeglich_oder_puenktlich == "ja":
+        return score
     else:
         return 0
 
 
-def get_score_total(a, b):
-    total_score = a + b
-    return total_score
+def get_score_total(total_ziel, total_berichtshet):
+    score = total_ziel + total_berichtshet
+    return score
 
 
-for file_name in input_dir:
+for file_name in input_dir:  # Execute this for all files in the input folder
     with open('input/' + file_name, 'r') as json_file:
         file_content = json.load(json_file)
 
@@ -38,12 +38,22 @@ for file_name in input_dir:
             current_puenktlich = current_berichtsheft["puenktlich"]
             current_taeglich = current_berichtsheft["taeglich"]
 
-            # Ziel:
+            # Calculate the score for "Ziel":
             total_wissen_anwenden = get_score_ziel(current_wissen_anwenden, 2)  # the second number is the weight
             total_etwas_gelernt = get_score_ziel(current_etwas_gelernt, 1)
             total_Kommunikation = get_score_ziel(current_Kommunikation, 2)
             total_eigeninitiative = get_score_ziel(current_eigeninitiative, 2)
             total_motivation = get_score_ziel(current_motivation, 1)
+
+            # Berichtsheft - the second parameter is the amount of points
+            total_puenktlich = get_score_berichtshet(current_puenktlich, 50)
+            total_taeglich = get_score_berichtshet(current_taeglich, 30)
+
+            total_ziel = total_wissen_anwenden + total_etwas_gelernt + total_Kommunikation + total_eigeninitiative + \
+                         total_motivation
+            total_berichtshet = total_puenktlich + total_taeglich
+
+            total_score = get_score_total(total_ziel, total_berichtshet)
 
             # Debugg:
             # print(current_wissen_anwenden, total_wissen_anwenden)
@@ -52,13 +62,4 @@ for file_name in input_dir:
             # print(current_eigeninitiative, total_eigeninitiative)
             # print(current_motivation, total_motivation)
 
-            # Berichtsheft - the second parameter is the amount of points
-            total_puenktlich = get_score_berichtshet(current_puenktlich, 50)
-            total_taeglich = get_score_berichtshet(current_taeglich, 30)
-
-            total_ziel = total_wissen_anwenden + total_etwas_gelernt + total_Kommunikation + total_eigeninitiative + total_motivation
-            total_berichtshet = total_puenktlich + total_taeglich
-
-            total_score = get_score_total(total_ziel, total_berichtshet)
-
-            print(total_score)
+            print(f"Score Berichtsheft: {total_berichtshet}, Score Ziel: {total_ziel} , Total Score: {total_score}")
